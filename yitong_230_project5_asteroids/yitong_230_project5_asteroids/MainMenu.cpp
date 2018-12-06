cfa8898d4e39c5473db52c5efddff9be011bb73a
YitongHuGpm20 <yhu216@ucsc.edu>
2018-12-05 17:06:09 -0800

    Main Menu done

--- yitong_230_project5_asteroids/yitong_230_project5_asteroids/MainMenu.cpp
+++ yitong_230_project5_asteroids/yitong_230_project5_asteroids/MainMenu.cpp
@@ -15,9 +15,16 @@
 using namespace sf;
 
 extern RenderWindow window;
+Text title;
+extern Font font;
 
-MainMenu::MainMenu():newGame(Vector2f(500, 300), "New Game"), quit(Vector2f(550, 400), "Quit") {
-	
+MainMenu::MainMenu():newGame(Vector2f(410, 400), "(^_^) New Game (^_^)"), quit(Vector2f(460, 500), "(^_^) Quit (^_^)") {
+	title.setFont(font);
+	title.setString("Yitong's Spaceship War Game");
+	title.setCharacterSize(64);
+	title.setFillColor(Color::Cyan);
+	title.setPosition(Vector2f(180, 100));
+	title.setStyle(Text::Italic);
 }
 
 AppState* MainMenu::update_state(float dt)
@@ -32,6 +39,7 @@ AppState* MainMenu::update_state(float dt)
 void MainMenu::render_frame()
 {
 	window.clear();
+	window.draw(title);
 	newGame.DrawButton();
 	quit.DrawButton();
 }
